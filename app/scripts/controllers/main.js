33c3b89db9d8f53cbf1b4a6be3717ae1aceb28b9
dan-lyn <daniel_lynge@hotmail.com>
2014-08-18 19:52:25 +0200

    setting active class on selected navigation item

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,6 +8,11 @@
  * Controller of the angularjsPlaygroundApp
  */
 angular.module('angularjsPlaygroundApp')
-  .controller('MainCtrl', function ($scope) {
-    // TODO
+  .controller('MainCtrl', function ($scope, $location) {
+
+    // Function to help set the active navigation item.
+    $scope.isActive = function(route) {
+        return route === $location.path();
+    }
+
   });
