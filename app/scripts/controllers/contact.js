34c7b41eb11479dd055fa4d71f0105f2be87ee0f
dan-lyn <daniel_lynge@hotmail.com>
2014-08-23 11:15:52 +0200

    setup contact form controller

--- app/scripts/controllers/contact.js
+++ app/scripts/controllers/contact.js
@@ -2,5 +2,18 @@
 
 angular.module('angularjsPlaygroundApp')
   .controller('ContactCtrl', function($scope) {
-    // TODO
+    $scope.result = 'hidden'
+    $scope.resultMessage;
+    $scope.formData;
+    $scope.submitButtonDisabled = false;
+    $scope.submitted = false;
+    $scope.submit = function(contactform) {
+      $scope.submitted = true;
+      $scope.submitButtonDisabled = true;
+      if (contactform.$valid) {
+        $scope.submitButtonDisabled = true;
+        $scope.resultMessage = "Your message was sent.";
+        $scope.result='bg-success';
+      }
+    }
   });
