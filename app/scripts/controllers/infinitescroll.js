c836f7a8e94a3f0d28a3c1776e3852355e0af726
dan-lyn <daniel_lynge@hotmail.com>
2014-09-12 16:44:25 +0200

    renamed function loadMore in loadMoreNumbers

--- app/scripts/controllers/infinitescroll.js
+++ app/scripts/controllers/infinitescroll.js
@@ -4,7 +4,7 @@ angular.module('angularjsPlaygroundApp')
   .controller('InfinitescrollCtrl', function($scope) {
     $scope.numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
 
-    $scope.loadMore = function() {
+    $scope.loadMoreNumbers = function() {
       var last = $scope.numbers[$scope.numbers.length-1];
       for (var i=1; i<=18; i++) {
         $scope.numbers.push(last+i);
