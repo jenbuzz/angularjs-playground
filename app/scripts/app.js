a2297426f5194453fa64318e5c078f1176db441d
dan-lyn <daniel_lynge@hotmail.com>
2014-09-12 16:35:17 +0200

    basic infinite scrolling with numbers

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,7 +15,8 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'infinite-scroll'
   ])
   .config(function ($routeProvider) {
     $routeProvider
--- app/scripts/controllers/infinitescroll.js
+++ app/scripts/controllers/infinitescroll.js
@@ -2,5 +2,12 @@
 
 angular.module('angularjsPlaygroundApp')
   .controller('InfinitescrollCtrl', function($scope) {
+    $scope.numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
 
+    $scope.loadMore = function() {
+      var last = $scope.numbers[$scope.numbers.length - 1];
+      for(var i = 1; i <= 18; i++) {
+        $scope.numbers.push(last + i);
+      }
+    };
   });
