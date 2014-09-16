bafdb2d2d325a1f1f17e593dd7525bd9dd801285
dan-lyn <daniel_lynge@hotmail.com>
2014-09-16 19:30:22 +0200

    setup carousel example (now working atm)

--- app/scripts/controllers/carousel.js
+++ app/scripts/controllers/carousel.js
@@ -2,6 +2,18 @@
 
 angular.module('angularjsPlaygroundApp')
   .controller('CarouselCtrl', function($scope) {
-    console.log("loading carousel controller");
+    $scope.myInterval = 5000;
+    var slides = $scope.slides = [];
+    $scope.addSlide = function() {
+      var newWidth = 600 + slides.length;
+      slides.push({
+        image: 'http://lorempixel.com/' + newWidth + '/300',
+        text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
+          ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+      });
+    };
+    for (var i=0; i<4; i++) {
+      $scope.addSlide();
+    }
   });
 
