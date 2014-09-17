8014ab2a218eb6db261c1f4d495a10b6b1d41ddd
dan-lyn <daniel_lynge@hotmail.com>
2014-09-17 07:35:14 +0200

    declared ui.bootstrap as a dependency

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,7 +16,8 @@ angular
     'ngRoute',
     'ngSanitize',
     'ngTouch',
-    'infinite-scroll'
+    'infinite-scroll',
+    'ui.bootstrap'
   ])
   .config(function ($routeProvider) {
     $routeProvider
