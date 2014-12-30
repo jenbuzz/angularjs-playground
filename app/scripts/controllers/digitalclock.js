bb5f9b8ef4efcd7712ad37c5a2996f991fdcbfbd
dan-lyn <daniel_lynge@hotmail.com>
2014-12-30 19:41:33 +0100

    styling of digital clock

--- app/scripts/controllers/digitalclock.js
+++ app/scripts/controllers/digitalclock.js
@@ -6,7 +6,7 @@ angular.module('angularjsPlaygroundApp')
     return {
       restrict: 'E',
       scope: {},
-      template: '<div ng-bind="now | date:\'HH:mm:ss\'"></div>',
+      template: '<div class="text-center clock" ng-bind="now | date:\'HH:mm:ss\'"></div>',
       link: function (scope) {
         scope.now = new Date();
         var clockTimer = $interval(function() {
