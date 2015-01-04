aa19191675e033af09cb7f407d817b8dd6ab41c9
dan-lyn <daniel_lynge@hotmail.com>
2015-01-04 16:17:58 +0100

    added custom validator functionality

--- app/scripts/controllers/customvalidator.js
+++ app/scripts/controllers/customvalidator.js
@@ -1,6 +1,36 @@
 'use strict';
 
 angular.module('angularjsPlaygroundApp')
-  .controller('CustomvalidatorCtrl', function($scope) {
+  .controller('CustomvalidatorCtrl', function($scope, $http) {
+    $scope.checkUniqueLogin = function(value) {
+      return $http.get('users.json').then(function(usersResponse) {
+        return !usersResponse.data.filter(function(user) {
+          return user.login === value;
+        }).length;
+      });
+    }
+  })
+  .directive('unique', function() {
+    return {
+      require: 'ngModel',
+      link: function(scope, element, attrs, ngModelCtrl) {
+        var original;
+
+        ngModelCtrl.$formatters.unshift(function(modelValue) {
+          original = modelValue;
+          return modelValue;
+        });
+
+        ngModelCtrl.$parsers.push(function (value) {
+          if (value && value !== original) {
+            scope[attrs.unique](value).then(function(result) {
+              ngModelCtrl.$setValidity('unique', result);
+            });
+
+            return value;
+          }
+        });
+      },
+    }
   });
 
