31ed27ca19f4f23be96c51a9dcf84c9c5814655e
dan-lyn <daniel_lynge@hotmail.com>
2015-08-09 12:27:03 +0200

    fixed main controller test

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -16,7 +16,8 @@ describe('Controller: MainCtrl', function () {
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
+  it('should include function isActive in scope', function () {
     expect(scope.awesomeThings.length).toBe(3);
+    expect(typeof scope.isActive).toBe("function")
   });
 });
