36e63442d7dee9f76c556e786626175d8d9df863
dan-lyn <daniel_lynge@hotmail.com>
2015-08-09 12:29:09 +0200

    added missing components to karma conf

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -26,6 +26,9 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
+      'bower_components/ngInfiniteScroll/build/ng-infinite-scroll.js',
+      'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
+      'bower_components/ngDialog/js/ngDialog.js',
       'app/scripts/**/*.js',
       'test/mock/**/*.js',
       'test/spec/**/*.js'
