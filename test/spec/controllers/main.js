97b04c2d8fe8f7099de0f01f5e03dc9ec990e825
dan-lyn <daniel_lynge@hotmail.com>
2015-08-16 13:11:17 +0200

    code style fixes

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -2,12 +2,10 @@
 
 describe('Controller: MainCtrl', function () {
 
-  // load the controller's module
   beforeEach(module('angularjsPlaygroundApp'));
 
   var MainCtrl, scope, location;
 
-  // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope, $location) {
     scope = $rootScope.$new();
     location = $location;
@@ -17,7 +15,7 @@ describe('Controller: MainCtrl', function () {
     });
   }));
 
-  it('should include function isActive in scope', function () {
+  it('should add function isActive to scope', function () {
     expect(angular.isFunction(scope.isActive)).toBe(true);
   });
 });
