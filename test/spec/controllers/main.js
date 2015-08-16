b3f2bb202a85049f5729744f34f96d9def8efac7
dan-lyn <daniel_lynge@hotmail.com>
2015-08-16 13:10:16 +0200

    fixed main controller unit test: testing for isactive function

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -5,18 +5,19 @@ describe('Controller: MainCtrl', function () {
   // load the controller's module
   beforeEach(module('angularjsPlaygroundApp'));
 
-  var MainCtrl,
-    scope;
+  var MainCtrl, scope, location;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function ($controller, $rootScope, $location) {
     scope = $rootScope.$new();
-    /*MainCtrl = $controller('MainCtrl', {
-      $scope: scope
-    });*/
+    location = $location;
+    MainCtrl = $controller('MainCtrl', {
+      $scope: scope,
+      $location: location
+    });
   }));
 
   it('should include function isActive in scope', function () {
-    // TODO: get controller instance how???
+    expect(angular.isFunction(scope.isActive)).toBe(true);
   });
 });
