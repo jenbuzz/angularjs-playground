2217515303a617e2a90ca42f835c0df05f524819
dan-lyn <daniel_lynge@hotmail.com>
2015-11-15 17:09:04 +0100

    example of templateurl usage

--- ng2/app/app.ts
+++ ng2/app/app.ts
@@ -4,7 +4,10 @@ import {Component, View} from 'angular2/angular2';
   selector: 'app'
 })
 @View({
-  template: '<h2>Welcome to the Playground Mr. {{ name }}<h2/>'
+  template: '<h2>Welcome to the Playground Mr. {{ name }}<h2/>',
+  
+  // Example of template file usage
+  // templateUrl: 'template.html'
 })
 export class App {
   constructor() {
