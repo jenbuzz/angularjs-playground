06f11965c1d53cea8a2cd93bacb8e5dfb827f0d0
dan-lyn <daniel_lynge@hotmail.com>
2017-01-29 08:57:35 +0100

    setup routing - one page

--- quickstart/app/app.module.ts
+++ quickstart/app/app.module.ts
@@ -1,10 +1,19 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports: [ 
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: AppComponent
+      }
+    ])
+  ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
