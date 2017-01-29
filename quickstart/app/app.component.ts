af854fc933a30e98a2bdd07278ff8501829b3896
dan-lyn <daniel_lynge@hotmail.com>
2017-01-29 08:06:06 +0100

    added a person class and setup usage of it in app

--- quickstart/app/app.component.ts
+++ quickstart/app/app.component.ts
@@ -1,7 +1,18 @@
 import { Component } from '@angular/core';
+import { Person } from './person'
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+    <h1>{{title}}</h1>
+    <h2>Hello {{person.name}}</h2>
+  `,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+  title = 'Angular Playground';
+  person: Person = {
+    id: 1,
+    name: 'John Doe',
+    email: 'johndoe@myemail.com'
+  }
+}
