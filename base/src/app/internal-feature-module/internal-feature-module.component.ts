7fc8b89ecadd3243208425e971439bde74e9c8a8
dan-lyn <daniel_lynge@hotmail.com>
2017-12-13 15:13:45 +0100

    added internal feature module

---
+++ base/src/app/internal-feature-module/internal-feature-module.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'internal-feature-module',
+  template: `
+      Hello from internal feature module!!
+  `
+})
+export class InternalFeatureModuleComponent {
+}
---
+++ base/src/app/internal-feature-module/internal-feature-module.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InternalFeatureModuleComponent } from './internal-feature-module.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    InternalFeatureModuleComponent
+  ],
+  exports: [
+    InternalFeatureModuleComponent  
+  ]
+})
+export class InternalFeatureModuleModule { }
