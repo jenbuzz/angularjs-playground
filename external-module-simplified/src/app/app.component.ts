d1f6b046b3c468b68e2893b643dce864eff4dd9a
dan-lyn <daniel_lynge@hotmail.com>
2017-12-21 19:39:25 +0100

    initial commit of external module simplified

---
+++ external-module-simplified/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+      Hello from external feature simplified module!!
+  `
+})
+export class AppComponent {
+}
