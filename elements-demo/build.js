364c92052ace1058be2e432934219b8e1c27573d
dan-lyn <daniel_lynge@hotmail.com>
2018-07-27 08:55:16 +0200

    removed unused stuff from build script

--- elements-demo/build.js
+++ elements-demo/build.js
@@ -1,7 +1,7 @@
 const fs = require('fs-extra');
 const concat = require('concat');
 
-(async function build() {
+(async () => {
     const files = [
         './dist/elements-demo/runtime.js',
         './dist/elements-demo/polyfills.js',
@@ -11,6 +11,4 @@ const concat = require('concat');
     await fs.ensureDir('elements')
     await concat(files, 'elements/custom-element.js');
     await fs.copyFile('./dist/elements-demo/styles.css', 'elements/styles.css')
-    await fs.copy('./dist/elements-demo/assets/', 'elements/assets/' )
-    
 })()
