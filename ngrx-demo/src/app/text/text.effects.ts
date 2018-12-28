96b67d2fa6f080d91a88c7fd4ff516acb34ece32
jenbuzz <daniel_lynge@hotmail.com>
2018-12-28 21:34:54 +0100

    ngrx-demo: updated text effects to use ofType pipe - function deprecated

--- ngrx-demo/src/app/text/text.effects.ts
+++ ngrx-demo/src/app/text/text.effects.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Effect, Actions } from '@ngrx/effects';
-import { Observable, of } from 'rxjs';
-import { map, concatMap } from 'rxjs/operators';
+import { Effect, Actions, ofType } from '@ngrx/effects';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import * as textActions from './text.actions';
 export type Action = textActions.All;
@@ -11,7 +11,8 @@ export class TextEffects {
     constructor(private actions$: Actions) {}
 
     @Effect()
-    editTextUpdate$: Observable<Action> = this.actions$.ofType(textActions.EDIT_TEXT).pipe(
+    editTextUpdate$: Observable<Action> = this.actions$.pipe(
+        ofType(textActions.EDIT_TEXT),
         map((action: textActions.EditText) => action.payload),
         map((text: string) => new textActions.IncrementTextCount())
     );
