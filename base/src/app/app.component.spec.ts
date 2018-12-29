d261056482fa4ad965a13f3550ba0156558cae80
jenbuzz <daniel_lynge@hotmail.com>
2018-12-29 09:28:25 +0100

    base: fixed app component test

--- base/src/app/app.component.spec.ts
+++ base/src/app/app.component.spec.ts
@@ -1,27 +1,34 @@
 import { TestBed, async } from '@angular/core/testing';
+import { MetafrenzyService } from 'ngx-metafrenzy';
+import { BabyComponent } from './baby/baby.component';
+import { BigDaddyComponent } from './big-daddy/big-daddy.component';
+import { BastardComponent } from './bastard/bastard.component';
+import { FamilyService } from './family.service';
 import { AppComponent } from './app.component';
+import { InternalFeatureModuleModule } from './internal-feature-module/internal-feature-module.module';
+
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                InternalFeatureModuleModule,
+            ],
+            declarations: [
+                AppComponent,
+                BabyComponent,
+                BigDaddyComponent,
+                BastardComponent,
+            ],
+            providers: [
+                MetafrenzyService,
+                FamilyService,
+            ],
+        }).compileComponents();
+    }));
+
+    it('should create the app', async(() => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.debugElement.componentInstance;
+        expect(app).toBeTruthy();
+    }));
 });
