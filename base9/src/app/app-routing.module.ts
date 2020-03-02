430e3f50d8e3a27021c26b4ac9c0257040b803bb
jenbuzz <daniel_lynge@hotmail.com>
2020-03-02 13:53:59 +0100

    base9: added ngx-metafrenzy v5

--- base9/src/app/app-routing.module.ts
+++ base9/src/app/app-routing.module.ts
@@ -1,8 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MetafrenzyGuard } from 'ngx-metafrenzy';
+import { AppComponent } from './app.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+    canActivate: [MetafrenzyGuard],
+    data: {
+        metafrenzy: {
+            tags: [
+                {
+                    name: 'og:title',
+                    content: 'Base Test'
+                }
+            ],
+            links: [
+                {
+                    rel: 'canonical',
+                    href: 'http://localhost/'
+                }
+            ]
+        }
+    }
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- base9/src/app/app.component.ts
+++ base9/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { MetafrenzyService } from 'ngx-metafrenzy';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [MetafrenzyService]
 })
 export class AppComponent {
   title = 'base9';
+
+  constructor(private metafrenzyService: MetafrenzyService) {
+    this.metafrenzyService.setTitle('Base Test');
+  }
 }
